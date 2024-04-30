0196789e6be6c7822c3373dbc80d13708cbc5404
six-shot <okhuomonajayi54@gmail.com>
2024-04-30 14:06:49 +0000

    navv

--- src/modules/home/feauture/hero/index.js
+++ src/modules/home/feauture/hero/index.js
@@ -7,10 +7,20 @@ import Slider from "../slider";
 export default function Hero() {
   return (
     <div>
-      <div className="bg-[#000000] w-full font-poppins pt-[]">
+      <div className="bg-[#000000] w-full font-poppins ">
         <div className="max-w-[1440px] mx-auto">
-          <div style={style.bg} className=""></div>
-          <Slider/>
+          <div style={style.bg} className="text-white">
+            <div className="w-full h-[55vh] flex flex-col gap-3 justify-center items-center">
+              <h2 className="text-center font-clash font-bold text-[40px] leading-[45px]">
+                Crafting Tommorrow's <br /> Innovations Today
+              </h2>
+              <p className="font-clash text-center">
+                The catalyst for your Web 3.0 venture to grow and thrive.<br/>The
+                catalyst for your Web 3.0 venture to 
+              </p>
+            </div>
+          </div>
+          <Slider />
         </div>
       </div>
     </div>
@@ -25,8 +35,7 @@ const style = {
     backgroundRepeat: "no-repeat",
     width: "100%",
     height: "70vh",
-    display: "flex",
-    alignItems: "center",
+    
     padding: "0px 5%",
     position: "relative",
   },
